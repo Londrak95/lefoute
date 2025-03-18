3f5b711d4524f8f419e3133c98d7786e70bb0946
Julien Gonzalez <julien.gonzalez@margo-group.net>
2025-03-18 16:21:53 +0100

    wip: add select to filter results

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -34,6 +34,7 @@ const columnsInfo = [
 
 export default function Results() {
 	const [results, setResults] = useState(null)
+	const [resultType, setResultType] = useState('all')
 
 	useEffect(
 		() => {
@@ -48,5 +49,20 @@ export default function Results() {
 
 	if (!results) return <div>Loading...</div>
 
-	return <Table columns={columnsInfo} initial_data={results}/>
+	return (
+		<>
+		<select name="result-types" id="result-type-select" value={resultType}
+		  onChange={(e) => {
+		  	// setResults(results.filter(result => result.goals_conceded === 0))
+		  	setResultType(e.target.value)
+		  }}>
+		  <option value="all">Tous les résultats</option>
+		  <option value="victory">Victoires</option>
+		  <option value="draw">Matchs Nuls</option>
+		  <option value="defeat">Défaites</option>
+		</select>
+		<hr/>
+		<Table columns={columnsInfo} initial_data={results}/>
+		</>
+	)
 }
