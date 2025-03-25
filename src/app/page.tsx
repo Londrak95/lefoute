adcade50cd6ef1a555824f024e267e7cf2816cf0
Julien Gonzalez <julien.gonzalez@margo-group.net>
2025-03-25 12:48:20 +0100

    add dropdown menu on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,41 @@
 'use client'
 
 import Link from 'next/link'
+import { useState } from 'react'
+
+const teams = ["Olympique de Marseille"]
+
+function DropDownListElement(props) {
+  const [spread, setSpread] = useState(false)
+
+  return (
+    <div
+      onMouseEnter={() => setSpread(true)}
+      onMouseLeave={() => setSpread(false)}
+    >
+      <>
+        <li> {props.label} </li>
+        {spread && (
+          <ul className="ml-3 max-w-md space-y-1 text-gray-500 list-[circle] list-inside dark:text-gray-400">
+            {props.choices.map((choice, index) =>
+              <li key={index}> <Link href={props.url} target={"_blank"}> {choice} </Link> </li>
+            )}
+          </ul>
+        )}
+      </>
+  </div>
+  )
+}
 
 export default function Home() {
   return (
-    <div className="mx-2">
+    <div className="mx-2 my-2">
     <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">P'tit projet vive lefoute</h2>
     <hr className="w-72 h-1 mx-2 my-4 bg-gray-900 border-0 rounded-sm md:my-5 dark:bg-gray-700"/>
     <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-      <li> <Link href="/standings" target="_blank">Classements Ligue 1</Link> </li>
-      <li> <Link href="/results" target="_blank">Résultats de l'OM</Link> </li>
-      <li> <Link href="/performers" target="_blank">Meilleurs buteurs/passeurs de l'OM</Link> </li>
+      <li> <Link href={"/standings"} target={"_blank"}> Classement Ligue 1 </Link> </li>
+      <DropDownListElement label={"Résultats"} url={"/results"} choices={teams}/>
+      <DropDownListElement label={"Meilleurs Buteurs et Passeurs"} url={"/performers"} choices={teams}/>
     </ul>
     </div>
   )
