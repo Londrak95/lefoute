9668480dcead56f5519dc28da7c7ae1df31e1ebe
Julien Gonzalez <julien.gonzalez@margo-group.net>
2025-04-01 17:43:55 +0200

    change how spread works on front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,17 @@ import LogoWithTitle from './components/logo_with_title'
 
 
 const teams = ["Olympique de Marseille"]
+const nbDropDownElement = 2
 
 function DropDownListElement(props) {
-  const [spread, setSpread] = useState(false)
-
   return (
     <div
-      onMouseEnter={() => setSpread(true)}
-      onMouseLeave={() => setSpread(false)}
+      onMouseLeave={props.onMouseLeave}
+      onMouseEnter={props.onMouseEnter}
     >
       <>
         <li> {props.label} </li>
-        {spread && (
+        {props.spread && (
           <ul className="ml-3 max-w-md space-y-1 text-gray-500">
             {props.choices.map((choice, index) =>
               <li className="flex flex-row" key={index}> <LogoWithTitle name={choice} link={props.url}/> </li>
@@ -30,14 +29,40 @@ function DropDownListElement(props) {
 }
 
 export default function Home() {
+  const [spreads, setSpreads] = useState(Array(nbDropDownElement).fill(false))
+
+  const handleSpreadEnter = (idx) => {
+    setSpreads(spreads.map((spread, i) => {
+      if (i === idx) {
+        return true
+      } else {
+        return false
+      }
+    }))
+  }
+
+  const handleSpreadLeave = () => setSpreads(Array(nbDropDownElement).fill(false))
+
   return (
     <div className="mx-2 my-2">
     <h2 className="mb-2 text-lg font-semibold text-gray-900 dark:text-white">P'tit projet vive lefoute</h2>
     <hr className="w-72 h-1 mx-2 my-4 bg-gray-900 border-0 rounded-sm md:my-5 dark:bg-gray-700"/>
     <ul className="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
       <li> <Link href={"/standings"} target={"_blank"}> Classement Ligue 1 </Link> </li>
-      <DropDownListElement label={"Résultats"} url={"/results"} choices={teams}/>
-      <DropDownListElement label={"Meilleurs Buteurs et Passeurs"} url={"/performers"} choices={teams}/>
+      <DropDownListElement
+        label={"Résultats"}
+        url={"/results"}
+        choices={teams}
+        spread={spreads[0]}
+        onMouseLeave={handleSpreadLeave}
+        onMouseEnter={(() => handleSpreadEnter(0))}/>
+      <DropDownListElement
+        label={"Meilleurs Buteurs et Passeurs"}
+        url={"/performers"}
+        choices={teams}
+        spread={spreads[1]}
+        onMouseLeave={handleSpreadLeave}
+        onMouseEnter={(() => handleSpreadEnter(1))}/>
     </ul>
     </div>
   )
